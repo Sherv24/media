1b1fb97c921c8df9aae35d6f1e218fbdc7ac77a2
Sherwin Sahebi <33410214+Sherv24@users.noreply.github.com>
2020-06-29 12:00:10 -0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Media{
+  int _numOfLikes;
+  User _user;
+  List<User> _likedBy;
+  List<Comment> _comments;
+  Board _board;
+  String _id;
+  Date _date;
+
+  int get _numOfLikes;
+  
+
+  //Getters
+
+
+  //Setters
+
+}
